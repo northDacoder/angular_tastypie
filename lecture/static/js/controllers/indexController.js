57a82e1bbd40e4f5249b42ddf9037897786dcc5a
John Ryan <jcryan85@gmail.com>
2014-03-11 16:09:22 -0700

    added Angular factor to provide access to all controllers

--- lecture/static/js/controllers/indexController.js
+++ lecture/static/js/controllers/indexController.js
@@ -9,6 +9,10 @@ function indexController($scope, $http, RocketSpace) {
         console.log(data.objects);
         $scope.students = data.objects;
         RocketSpace.student_list = $scope.students;
+    });
 
-    })
+    $http.get('api/v1/class/?format=json').success(function(data){
+        console.log(data.objects);
+        $scope.classes = data.objects;
+    });
 }
