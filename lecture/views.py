53e85af8614dd59f63820c3a723c716b99031410
John Ryan <jcryan85@gmail.com>
2014-03-11 10:11:50 -0700

    added static files for angular templates

--- lecture/views.py
+++ lecture/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+def angular(request):
+    return render(request, "angular.html")
 
 def presentation(request):
     return render(request, "presentation.html")
