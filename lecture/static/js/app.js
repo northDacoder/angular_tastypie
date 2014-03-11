c6c635b655b22430c4f0316f9ff39b5be7e63be8
John Ryan <jcryan85@gmail.com>
2014-03-11 13:49:04 -0700

    added more routes

--- lecture/static/js/app.js
+++ lecture/static/js/app.js
@@ -5,19 +5,7 @@ var app = angular.module('exampleApp', [
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
-        when('/',
-            {
-                templateUrl: '/static/views/index.html',
-                controller: 'indexController'
-            }).
-        when('/class',
-            {
-                templateUrl: '/static/views/class.html',
-                controller: 'classController'
-            }).
-        when('/student',
-            {
-                templateUrl: '/static/views/student.html',
-                controller: 'studentController'
-            });
-)]);
+        when('/',{ templateUrl: '/static/views/index.html', controller: 'indexController' }).
+        when('/class',{ templateUrl: '/static/views/class.html', controller: 'classController' }).
+        when('/student', { templateUrl: '/static/views/student.html', controller: 'studentController' })
+}]);
--- lecture/static/js/controllers/classController.js
+++ lecture/static/js/controllers/classController.js
@@ -1,5 +1,7 @@
-function classControllerController($scope, $http, RocketSpace) {
-
-    console.log(RocketSpace.angular_instructor);
-    console.log("classController");
-}
+function classController($scope, $http) {
+      $http.get('api/v1/class/?format=json').success(function(data){
+            console.log(data);
+      }).error(function(data){
+            console.log("You have an error in your code");
+      });
+};
