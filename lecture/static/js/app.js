3667cf42dbc7ecc4050ccc260d1e29831ddbba1f
John Ryan <jcryan85@gmail.com>
2014-03-11 20:27:12 -0700

    structured files

--- lecture/static/js/app.js
+++ lecture/static/js/app.js
@@ -6,7 +6,7 @@ var app = angular.module('exampleApp', [
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $routeProvider.
         when('/',{ templateUrl: '/static/views/index.html', controller: 'indexController' }).
-        when('/class/:id',{ templateUrl: '/static/views/class.html', controller: 'classController' }).
+        when('/class',{ templateUrl: '/static/views/class.html', controller: 'classController' }).
         when('/student', { templateUrl: '/static/views/student.html', controller: 'studentController' }).
         when('/addStudent', { templateUrl: '/static/views/add_student.html', controller: 'studentController' })
 }]);
